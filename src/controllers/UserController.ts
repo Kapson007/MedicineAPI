08728e49c960e0d4a63e38814138e5bafc6294c0
Kacper Nadstoga <kapix007@gmail.com>
2023-04-19 23:29:37 +0200

    create user model and controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+import {UserModel} from '../schemas/users';
+
+export const getUsers = () => UserModel.find();
+export const getUserByMail = (email: string) => UserModel.findOne({email});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = async (values: Record<string, any>) => new UserModel(values).save().then(user => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({_id: id});
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
