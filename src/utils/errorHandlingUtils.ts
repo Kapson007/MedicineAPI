24f5c650d9e0a0434d839ed9e5c76f5d55caf985
Kacper Nadstoga <kapix007@gmail.com>
2023-04-26 21:53:09 +0200

    extended error handling

--- src/utils/errorHandlingUtils.ts
+++ src/utils/errorHandlingUtils.ts
@@ -1,17 +1,21 @@
 import {Response} from 'express';
+
 type ErrorMessage = "Not Found" | "Bad Request";
 
 export const handleError = (response: unknown, errMessage: ErrorMessage) => {
-    if(!response) throw Error(errMessage)
+    if (!response) throw Error(errMessage)
 }
 
 export const errorMachine = (res: Response, err: Error) => {
-    switch (err.message){
+    switch (err.message) {
         case "Not Found":
-            res.status(404).json({message: err.message, code:404}).end();
+            res.status(404).json({message: err.message, code: 404}).end();
+            break;
+        case "Bad Request":
+            res.status(400).json({message: "Bad Request", code: 400}).end();
             break;
         default:
-            res.status(400).json({message: "Bad Request", code: 400}).end();
+            res.status(500).json({message: "Internal Server Error", code: 500}).end();
             break;
     }
 }
